2292789b2e790a3cc2e222f2d69dc43f9c053136
Matjaž Cerkvenik <matjaz99@gmail.com>
2020-02-15 10:56:40 +0100

    added in_swarm flag

--- node-exporter/conf/docker-entrypoint.sh
+++ node-exporter/conf/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 NODE_NAME=$(cat /etc/nodename)
-echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /file-collector/node-meta.prom
+echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\", in_swarm=\"true\"} 1" > /file-collector/node-meta.prom
 
 set -- /bin/node_exporter "$@"
 
